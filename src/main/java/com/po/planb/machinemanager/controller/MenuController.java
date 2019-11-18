eea5806ba041db17741d5f3f77497962219c0729
Bentozord <rafal.utnicki26@gmail.com>
2019-11-18 21:57:00 +0100

    revert

--- src/main/java/com/po/planb/machinemanager/controller/MenuController.java
+++ src/main/java/com/po/planb/machinemanager/controller/MenuController.java
@@ -10,26 +10,26 @@ import javax.validation.Valid;
 
 @Controller
 public class MenuController {
-//
-//    @RequestMapping("/")
-//    public String menu(@ModelAttribute(name = "supplierId") @Valid String supplierId, Model model) {
-//        if (supplierId.isEmpty()) {
-//            return "redirect:/login"; //TODO shouldn't put any logic here(remove when OAuth is implemented)
-//        } else {
-//            model.addAttribute("supplierId", supplierId);
-//            return "menu";
-//        }
-//    }
-//
-//    @RequestMapping("/create")
-//    public String create(@RequestParam(name = "id") @Valid String id, Model model) {
-//        model.addAttribute("id", id);
-//        return "createMachine";
-//    }
-//
-//    @RequestMapping("/login")
-//    public String login() {
-//        return "login";
-//    }
+
+    @RequestMapping("/")
+    public String menu(@ModelAttribute(name = "supplierId") @Valid String supplierId, Model model) {
+        if (supplierId.isEmpty()) {
+            return "redirect:/login"; //TODO shouldn't put any logic here(remove when OAuth is implemented)
+        } else {
+            model.addAttribute("supplierId", supplierId);
+            return "menu";
+        }
+    }
+
+    @RequestMapping("/create")
+    public String create(@RequestParam(name = "id") @Valid String id, Model model) {
+        model.addAttribute("id", id);
+        return "createMachine";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
 
 }
