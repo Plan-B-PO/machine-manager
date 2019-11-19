3df04ef3a2aaca337456056d338811f9c8499420
Bentozord <rafal.utnicki26@gmail.com>
2019-11-19 17:39:05 +0100

    restcontroller

--- src/main/java/com/po/planb/machinemanager/controller/MachinesController.java
+++ src/main/java/com/po/planb/machinemanager/controller/MachinesController.java
@@ -5,13 +5,13 @@ import com.po.planb.machinemanager.model.MachineDetails;
 import com.po.planb.machinemanager.service.MachinesService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@Controller
+@RestController
 @RequestMapping("/machine-manager/machines")
 public class MachinesController {
 
