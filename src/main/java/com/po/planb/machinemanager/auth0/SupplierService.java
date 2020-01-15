83a9f4e510c427eb97b04391fcadb19f2412af94
Bentozord <rafal.utnicki26@gmail.com>
2020-01-15 12:35:42 +0100

    get and retrieve role from Auth0 API.

--- src/main/java/com/po/planb/machinemanager/auth0/SupplierService.java
+++ src/main/java/com/po/planb/machinemanager/auth0/SupplierService.java
@@ -5,13 +5,16 @@ import com.auth0.exception.APIException;
 import com.auth0.exception.Auth0Exception;
 import com.auth0.json.auth.UserInfo;
 import com.auth0.net.Request;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.po.planb.machinemanager.model.role.Role;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Map;
 
 @Service
@@ -61,13 +64,21 @@ public class SupplierService {
             UserInfo info = request.execute();
             Map<String, Object> values = info.getValues();
             String sub = values.get("sub").toString();
-            HttpResponse<String> response = null;
+            HttpResponse<String> response;
             try {
                 response = Unirest.get("https://utnickir.eu.auth0.com/api/v2/users/" + sub + "/roles")
                         .header("authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5UWTROVFZGTVRORVJEVkJOVE16UkRBNU5rWkZORGt3UWpNeU5qSXpSVFUzUVRCQ1JrSkdOdyJ9.eyJpc3MiOiJodHRwczovL3V0bmlja2lyLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJiYTlKcEZTS05lUTB2MWFmdWpoOGszMDE1MWl3YTRvSkBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly91dG5pY2tpci5ldS5hdXRoMC5jb20vYXBpL3YyLyIsImlhdCI6MTU3OTA0MTc0MCwiZXhwIjoxNTgxNjMzNzQwLCJhenAiOiJiYTlKcEZTS05lUTB2MWFmdWpoOGszMDE1MWl3YTRvSiIsInNjb3BlIjoicmVhZDpjbGllbnRfZ3JhbnRzIGNyZWF0ZTpjbGllbnRfZ3JhbnRzIGRlbGV0ZTpjbGllbnRfZ3JhbnRzIHVwZGF0ZTpjbGllbnRfZ3JhbnRzIHJlYWQ6dXNlcnMgdXBkYXRlOnVzZXJzIGRlbGV0ZTp1c2VycyBjcmVhdGU6dXNlcnMgcmVhZDp1c2Vyc19hcHBfbWV0YWRhdGEgdXBkYXRlOnVzZXJzX2FwcF9tZXRhZGF0YSBkZWxldGU6dXNlcnNfYXBwX21ldGFkYXRhIGNyZWF0ZTp1c2Vyc19hcHBfbWV0YWRhdGEgY3JlYXRlOnVzZXJfdGlja2V0cyByZWFkOmNsaWVudHMgdXBkYXRlOmNsaWVudHMgZGVsZXRlOmNsaWVudHMgY3JlYXRlOmNsaWVudHMgcmVhZDpjbGllbnRfa2V5cyB1cGRhdGU6Y2xpZW50X2tleXMgZGVsZXRlOmNsaWVudF9rZXlzIGNyZWF0ZTpjbGllbnRfa2V5cyByZWFkOmNvbm5lY3Rpb25zIHVwZGF0ZTpjb25uZWN0aW9ucyBkZWxldGU6Y29ubmVjdGlvbnMgY3JlYXRlOmNvbm5lY3Rpb25zIHJlYWQ6cmVzb3VyY2Vfc2VydmVycyB1cGRhdGU6cmVzb3VyY2Vfc2VydmVycyBkZWxldGU6cmVzb3VyY2Vfc2VydmVycyBjcmVhdGU6cmVzb3VyY2Vfc2VydmVycyByZWFkOmRldmljZV9jcmVkZW50aWFscyB1cGRhdGU6ZGV2aWNlX2NyZWRlbnRpYWxzIGRlbGV0ZTpkZXZpY2VfY3JlZGVudGlhbHMgY3JlYXRlOmRldmljZV9jcmVkZW50aWFscyByZWFkOnJ1bGVzIHVwZGF0ZTpydWxlcyBkZWxldGU6cnVsZXMgY3JlYXRlOnJ1bGVzIHJlYWQ6cnVsZXNfY29uZmlncyB1cGRhdGU6cnVsZXNfY29uZmlncyBkZWxldGU6cnVsZXNfY29uZmlncyByZWFkOmhvb2tzIHVwZGF0ZTpob29rcyBkZWxldGU6aG9va3MgY3JlYXRlOmhvb2tzIHJlYWQ6ZW1haWxfcHJvdmlkZXIgdXBkYXRlOmVtYWlsX3Byb3ZpZGVyIGRlbGV0ZTplbWFpbF9wcm92aWRlciBjcmVhdGU6ZW1haWxfcHJvdmlkZXIgYmxhY2tsaXN0OnRva2VucyByZWFkOnN0YXRzIHJlYWQ6dGVuYW50X3NldHRpbmdzIHVwZGF0ZTp0ZW5hbnRfc2V0dGluZ3MgcmVhZDpsb2dzIHJlYWQ6c2hpZWxkcyBjcmVhdGU6c2hpZWxkcyBkZWxldGU6c2hpZWxkcyByZWFkOmFub21hbHlfYmxvY2tzIGRlbGV0ZTphbm9tYWx5X2Jsb2NrcyB1cGRhdGU6dHJpZ2dlcnMgcmVhZDp0cmlnZ2VycyByZWFkOmdyYW50cyBkZWxldGU6Z3JhbnRzIHJlYWQ6Z3VhcmRpYW5fZmFjdG9ycyB1cGRhdGU6Z3VhcmRpYW5fZmFjdG9ycyByZWFkOmd1YXJkaWFuX2Vucm9sbG1lbnRzIGRlbGV0ZTpndWFyZGlhbl9lbnJvbGxtZW50cyBjcmVhdGU6Z3VhcmRpYW5fZW5yb2xsbWVudF90aWNrZXRzIHJlYWQ6dXNlcl9pZHBfdG9rZW5zIGNyZWF0ZTpwYXNzd29yZHNfY2hlY2tpbmdfam9iIGRlbGV0ZTpwYXNzd29yZHNfY2hlY2tpbmdfam9iIHJlYWQ6Y3VzdG9tX2RvbWFpbnMgZGVsZXRlOmN1c3RvbV9kb21haW5zIGNyZWF0ZTpjdXN0b21fZG9tYWlucyByZWFkOmVtYWlsX3RlbXBsYXRlcyBjcmVhdGU6ZW1haWxfdGVtcGxhdGVzIHVwZGF0ZTplbWFpbF90ZW1wbGF0ZXMgcmVhZDptZmFfcG9saWNpZXMgdXBkYXRlOm1mYV9wb2xpY2llcyByZWFkOnJvbGVzIGNyZWF0ZTpyb2xlcyBkZWxldGU6cm9sZXMgdXBkYXRlOnJvbGVzIHJlYWQ6cHJvbXB0cyB1cGRhdGU6cHJvbXB0cyByZWFkOmJyYW5kaW5nIHVwZGF0ZTpicmFuZGluZyByZWFkOmxvZ19zdHJlYW1zIGNyZWF0ZTpsb2dfc3RyZWFtcyBkZWxldGU6bG9nX3N0cmVhbXMgdXBkYXRlOmxvZ19zdHJlYW1zIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.pfVEoTfa7svvHD3GdkEzFnA_S0krIFDeN6hS6glMwySi9rWBFttHVlu3OI_9aJlT8xOSWwm7X3W3_3EBPxOD5e0tslTmHLlMWYaZFmwPml4VH3BmarqEwhZe5k0bXIPDZOiqp49U5ywVkQLwZPUCoK_VbFhjy7_2evVfmIG8dsfqLa1umKmghOyQN6pXJni7Bjq9kevmGoeGUpYKAt28MYD-DMxwKJesMrsRG2N77w-zxVEoeMrlLDDC7PDdZ-93VSXeiUuhktT_PUkv4t92f19OV0nVUyIVBM8wwa9DxP3GlKdnoqCDn2djQFQfLPQGYg3LzeAFCe-tsNOv-OTwWg")
                         .asString();
-                System.out.println("ROLE ============== " + response.getBody());
-                return response.getBody();
+                String body = response.getBody();
+                try {
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    Role role = objectMapper.readValue(body, Role.class);
+                    System.out.println("ROLE ================ " + role.getName());
+                    return role.getName();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return body;
             } catch (UnirestException e) {
                 e.printStackTrace();
                 throw new IllegalStateException("Role API Exception");
---
+++ src/main/java/com/po/planb/machinemanager/model/role/Role.java
@@ -0,0 +1,18 @@
+package com.po.planb.machinemanager.model.role;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Role {
+    String id;
+    String name;
+    String description;
+    Sources sources;
+}
